40830fbbb473c15c8beb44c0574057b40ef2a9db
ovoskresenskiy <7949894@ukr.net>
2022-12-28 00:29:50 +0200

    Car Mapper

---
+++ src/main/java/com/example/hillel_garage_v2/repository/mapper/CarMapper.java
@@ -0,0 +1,20 @@
+package com.example.hillel_garage_v2.repository.mapper;
+
+import com.example.hillel_garage_v2.model.Car;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CarMapper implements RowMapper<Car> {
+
+    @Override
+    public Car mapRow(ResultSet rows, int rowNum) throws SQLException {
+        return Car.builder()
+                .id(rows.getInt("id"))
+                .model(rows.getString("model"))
+                .color(rows.getString("color"))
+                .ownerID(rows.getInt("ownerID"))
+                .build();
+    }
+}
