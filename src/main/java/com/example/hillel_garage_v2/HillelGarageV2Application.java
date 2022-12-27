1d3f56646b0414c9131a09492a7ac7e915ef2ece
ovoskresenskiy <7949894@ukr.net>
2022-12-27 11:58:51 +0200

    Init commit

---
+++ src/main/java/com/example/hillel_garage_v2/HillelGarageV2Application.java
@@ -0,0 +1,13 @@
+package com.example.hillel_garage_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HillelGarageV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HillelGarageV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hillel_garage_v2/HillelGarageV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hillel_garage_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HillelGarageV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
