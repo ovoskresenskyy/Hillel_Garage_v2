4af768f409387bb121da5a300f2223c683e380aa
ovoskresenskyi <7949894@ukr.net>
2023-01-02 15:22:32 +0200

    Code cleaning

--- src/main/java/com/example/hillel_garage_v2/controller/CarController.java
+++ src/main/java/com/example/hillel_garage_v2/controller/CarController.java
@@ -1,7 +1,9 @@
 package com.example.hillel_garage_v2.controller;
 
 import com.example.hillel_garage_v2.model.Car;
+import com.example.hillel_garage_v2.model.Owner;
 import com.example.hillel_garage_v2.service.CarService;
+import com.example.hillel_garage_v2.service.OwnerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,10 +13,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/cars")
 public class CarController {
 
+    private final OwnerService ownerService;
     private final CarService carService;
 
     @Autowired
-    public CarController(CarService carService) {
+    public CarController(OwnerService ownerService, CarService carService) {
+        this.ownerService = ownerService;
         this.carService = carService;
     }
 
