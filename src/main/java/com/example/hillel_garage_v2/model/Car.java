99a861491fa88bd072264fb11ff17551949e498d
ovoskresenskyi <7949894@ukr.net>
2023-01-08 01:40:03 +0200

    Added annotations ID

--- src/main/java/com/example/hillel_garage_v2/model/Car.java
+++ src/main/java/com/example/hillel_garage_v2/model/Car.java
@@ -2,10 +2,12 @@ package com.example.hillel_garage_v2.model;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 @Builder
 public class Car {
+    @Id
     private int id;
     private String model;
     private String color;
