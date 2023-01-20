4518dbac70f4362e1f2ac3a093ef7351fcef5dff
ovoskresenskyi <7949894@ukr.net>
2023-01-20 15:27:27 +0200

    Added annotation Table. To enable work with multiply databases

--- src/main/java/com/example/hillel_garage_v2/model/Car.java
+++ src/main/java/com/example/hillel_garage_v2/model/Car.java
@@ -3,9 +3,11 @@ package com.example.hillel_garage_v2.model;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Builder
+@Table("car")
 public class Car {
     @Id
     private int id;
--- src/main/java/com/example/hillel_garage_v2/model/Owner.java
+++ src/main/java/com/example/hillel_garage_v2/model/Owner.java
@@ -3,9 +3,11 @@ package com.example.hillel_garage_v2.model;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Builder
+@Table("owner")
 public class Owner {
     @Id
     private int id;
