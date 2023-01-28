7da9df4b38830c295fa7692e13d8220dac27bb97
ovoskresenskyi <7949894@ukr.net>
2023-01-28 02:48:45 +0200

    Reworked migration. Added table of roles

--- src/main/resources/db/migration/V1_02__creating_table_of_users.sql
+++ src/main/resources/db/migration/V1_02__creating_table_of_users.sql
@@ -1,9 +1,8 @@
-CREATE TABLE public.session_user
+CREATE TABLE public.user
 (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 ),
     name text,
     email text,
     password text,
-    role text,
     PRIMARY KEY (id)
 );
