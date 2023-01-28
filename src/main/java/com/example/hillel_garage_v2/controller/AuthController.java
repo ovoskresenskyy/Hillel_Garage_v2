8cc11f536e42182dd5d2b70020c09a9ead174d19
ovoskresenskyi <7949894@ukr.net>
2023-01-28 02:53:57 +0200

    Minor fixes of AuthController

--- src/main/java/com/example/hillel_garage_v2/controller/AuthController.java
+++ src/main/java/com/example/hillel_garage_v2/controller/AuthController.java
@@ -1,13 +1,11 @@
 package com.example.hillel_garage_v2.controller;
 
-import com.example.hillel_garage_v2.entity.SessionUser;
-import com.example.hillel_garage_v2.enums.Role;
-import com.example.hillel_garage_v2.service.SessionUserService;
+import com.example.hillel_garage_v2.entity.User;
+import com.example.hillel_garage_v2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,11 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class AuthController {
 
-    private final SessionUserService sessionUserService;
+    private final UserService userService;
 
     @Autowired
-    public AuthController(SessionUserService sessionUserService) {
-        this.sessionUserService = sessionUserService;
+    public AuthController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/index")
@@ -34,34 +32,33 @@ public class AuthController {
 
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
-        model.addAttribute("user", SessionUser.builder()
-                .role(Role.ADMIN)
+        model.addAttribute("user", User.builder()
                 .build());
         return "security/register";
     }
 
     @PostMapping("/register/save")
-    public String registration(@Validated @ModelAttribute("user") SessionUser sessionUser, BindingResult result, Model model) {
+    public String registration(@ModelAttribute("user") User user, BindingResult result, Model model) {
 
-        SessionUser existingSessionUser = sessionUserService.findByEmail(sessionUser.getEmail());
-        if (existingSessionUser != null
-                && existingSessionUser.getEmail() != null
-                && !existingSessionUser.getEmail().isEmpty()) {
+        User existingUser = userService.findByEmail(user.getEmail());
+        if (existingUser != null
+                && existingUser.getEmail() != null
+                && !existingUser.getEmail().isEmpty()) {
             result.rejectValue("email", "IsExist", "There is already an account registered with the same email");
         }
 
         if (result.hasErrors()) {
-            model.addAttribute("user", sessionUser);
+            model.addAttribute("user", user);
             return "/register";
         }
 
-        sessionUserService.saveUser(sessionUser);
-        return "redirect:/register?success";
+        userService.saveUser(user);
+        return "redirect:/login";
     }
 
     @GetMapping("/users")
     public String users(Model model) {
-        model.addAttribute("users", sessionUserService.findAll());
+        model.addAttribute("users", userService.findAll());
         return "security/users";
     }
 }
